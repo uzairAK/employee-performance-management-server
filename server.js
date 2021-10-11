0bfda86aa4ea56727feb1d94c6a15ddf05541860
uzairAK <uzairaezad97@gmail.com>
2021-10-11 10:59:38 +0500

    resolved accurate goals score issue

--- server.js
+++ server.js
@@ -58,81 +58,8 @@ app.use("/cycle_calculate", cycle_calculate);
 app.use("/hr", hr);
 
 app.get("/", (req, res) => {
-  // const goal = new Goal({
-  //   title: "Ian Fleming",
-  //   description: "description",
-  //   goal_start: "goal start",
-  //   end_period: "end period",
-  //   reviewer_id: "reviewer_id",
-  // });
-
-  // const goal_progress = new GoalProgress({
-  //   comment: "Casino Royale comment 4",
-  //   progress: 4,
-  //   goal: goal._id, // assign the _id from the goal
-  // });
-
-  // const goal_review = new GoalReview({
-  //   comment: "Casino Royale comment 4",
-  //   rating: 4,
-  // });
-  // goal.goal_progresses.push(goal_progress._id);
-  // goal.goal_review = goal_review._id;
-
-  // goal_progress.save(function (err) {
-  //   if (err) return console.log(err);
-  //   goal_review.save((err, review) => {
-  //     if (err) return console.log(err);
-  //     goal.save(function (err, goal) {
-  //       if (err) return consoloe.log(err);
-  //       console.log("inserted");
-
-  //       Goal.find({})
-  //         .populate("goal_progresses")
-  //         .populate("goal_review")
-  //         .exec(function (err, product) {
-  //           if (err) {
-  //             return console.log(err);
-  //           } else {
-  //             res.json(product);
-  //           }
-  //         });
-  //     });
-  //   });
-  // });
-
-  res.send("hello from simple server :)");
+  res.send("hello from Employee Performance server :)");
 });
-
-
-
-// sendErrorToMunshi();
-// console.log(__dirname);
-// console.log("Main");
-// console.log(path.dirname(__dirname));
-// console.log(path.basename(__dirname));
-// console.log(path.resolve(__dirname));
-// console.log(path.normalize("../"));
-// console.log("Main2");
-// console.log(path.dirname(__filename));
-// console.log(path.basename(__filename));
-// console.log(path.resolve(__filename));
-
-// console.log("\n\n");
-// console.log(path.join("/", path.basename(__dirname) ));
-
-// var data = {
-//   origin_trace : {FilePath: path.join("/", path.basename(__dirname) ), FileName: path.basename(__filename), TechLog: ""},
-//   other_data: {},
-//   nonblocking: 1,
-//   one_id: "000000",
-//   app_id: "10",
-//   case: "SYSTEM_LOGS",
-//   error_code: 0,
-//   error_title: "Testing from Node Function",
-// }
-// logError(data);
-// sendErrorToMunshi(data);
 
 app.listen(port, () =>
   console.log("> Server is up and running on port : " + port)
---
+++ test.js
@@ -0,0 +1,28 @@
+
+// sendErrorToMunshi();
+// console.log(__dirname);
+// console.log("Main");
+// console.log(path.dirname(__dirname));
+// console.log(path.basename(__dirname));
+// console.log(path.resolve(__dirname));
+// console.log(path.normalize("../"));
+// console.log("Main2");
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.resolve(__filename));
+
+// console.log("\n\n");
+// console.log(path.join("/", path.basename(__dirname) ));
+
+// var data = {
+//   origin_trace : {FilePath: path.join("/", path.basename(__dirname) ), FileName: path.basename(__filename), TechLog: ""},
+//   other_data: {},
+//   nonblocking: 1,
+//   one_id: "000000",
+//   app_id: "10",
+//   case: "SYSTEM_LOGS",
+//   error_code: 0,
+//   error_title: "Testing from Node Function",
+// }
+// logError(data);
+// sendErrorToMunshi(data);
